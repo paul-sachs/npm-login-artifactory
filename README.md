a01273272f6f64b1ddb419ba667e1b6008b216a3
Paul Sachs <paul.sachs@ibm.com>
2019-02-19 15:09:31 -0500

    Added command line args

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
+# Purpose
+
+Simplify configuring your npmrc file for using artifactory credentials with npm. It is designed to be run as a CLI, installed through npm or npx:
+
+```bash
+npx git+ssh://git@github.ibm.com:watson-finance/npm-login-artifactory.git
+```
+
+or
+
+```bash
+npm i -g git+ssh://git@github.ibm.com:watson-finance/npm-login-artifactory.git
+npm-login-artifactory
+```
+
 # Config
+
+There are a couple of ways to use the tool, from command line args to a config file. To see command line args, run `npm-login-artifactory -h`.
 
 You can preconfigure an npmartrc so the tool can read some defaults:
 
--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
-const readline = require("readline");
 const util = require("util");
 const prompts = require("prompts");
 const axios = require("axios");
+const optimist = require("optimist");
 
 const q = text => new Promise(resolve => rl.question(text, resolve));
 const existsP = util.promisify(fs.exists);
@@ -15,6 +15,56 @@ const writeFileP = util.promisify(fs.writeFile);
 
 const outputFilePath = path.resolve(".npmrc");
 const configFilePath = path.resolve(".npmartrc");
+
+const command = optimist
+  .usage(
+    "Customize your npm credentials to support artifactory.\nUsage: npm-login-artifactory"
+  )
+  .options("n", {
+    alias: "hostname",
+    describe: "Artifactory hostname. Eg: na.artifactory.swg-devops.com"
+  })
+  .options("r", {
+    alias: "registries",
+    describe:
+      "Comma seperated list of registries to map namespaces to. Eg: @fss=>ip-wfss-npm-virtual"
+  })
+  .options("e", {
+    alias: "email",
+    describe: "Intranet email"
+  })
+  .options("p", {
+    alias: "password",
+    describe: "Intranet password"
+  })
+  .boolean("x")
+  .options("x", {
+    alias: "skipApiKey",
+    describe: "Don't use api key for auth, just use password"
+  })
+  .boolean("q")
+  .options("q", {
+    describe: "Quiet mode"
+  })
+  .options("h", {
+    alias: "help",
+    describe: "Provide usage information"
+  });
+
+const argv = command.argv;
+
+if (argv.h) {
+  command.showHelp(console.log);
+  return;
+}
+
+const consoleConfig = {
+  hostname: argv.n,
+  registries: argv.r ? argv.r.split(",") : [],
+  email: argv.e,
+  password: argv.p,
+  useApiKey: !argv.x
+};
 
 const main = async () => {
   let config = {};
@@ -29,66 +79,83 @@ const main = async () => {
       console.warn("Could not read config: ", e);
     }
   }
-  let finalConfig;
-  try {
-    finalConfig = await prompts([
-      {
-        name: "email",
-        initial: config.email,
-        type: "text", //() => (config.email ? null : "text"),
-        message: "Enter your intranet email",
-        validate: i => !!i
-      },
-      {
-        name: "password",
-        initial: config.password,
-        type: "invisible", //() => (config.password ? null : "invisible"),
-        message: "Enter your intranet password",
-        validate: i => !!i
-      },
-      {
-        name: "hostname",
-        initial: config.hostname,
-        type: "text", //() => (config.hostname ? null : "text",
-        message: "Artifactory hostname",
-        validate: i => !!i
-      },
-      {
-        name: "registries",
-        initial: config.registries ? config.registries.join(",") : "",
-        type: "list",
-        message: "Enter registries",
-        validate: value =>
-          /^@[\w-_]+=>[\w-_]+/.test(value) ||
-          "Invalid format. Values must be @<scope>=><repo>. Eg, @fss/ip-wfss-npm-virtual"
-      },
-      {
-        name: "useApiKey",
-        initial: true,
-        active: "yes",
-        inactive: "no",
-        type: "toggle",
-        message: "Use API Key instead. Will create it if it doesn't exist."
+  // Merge command line args with config file
+  config = {
+    ...consoleConfig,
+    ...config
+  };
+  if (!argv.q) {
+    try {
+        config = await prompts([
+        {
+          name: "email",
+          initial: config.email,
+          type: "text",
+          message: "Enter your intranet email",
+          validate: i => !!i
+        },
+        {
+          name: "password",
+          initial: config.password,
+          type: "invisible",
+          message: "Enter your intranet password",
+          validate: i => !!i
+        },
+        {
+          name: "hostname",
+          initial: config.hostname,
+          type: "text",
+          message: "Artifactory hostname",
+          validate: i => !!i
+        },
+        {
+          name: "registries",
+          initial: config.registries ? config.registries.join(",") : "",
+          type: "list",
+          message: "Enter registries",
+          validate: value =>
+            /^@[\w-_]+=>[\w-_]+/.test(value) ||
+            "Invalid format. Values must be @<scope>=><repo>. Eg, @fss/ip-wfss-npm-virtual"
+        },
+        {
+          name: "useApiKey",
+          initial: config.useApiKey,
+          active: "yes",
+          inactive: "no",
+          type: "toggle",
+          message: "Use API Key instead. Will create it if it doesn't exist."
+        }
+      ]);
+    } catch (e) {
+      console.log("Aborting due to:", e);
+      return;
+    }
+  } else {
+      // validate that the config has everything we need
+      const missingParams = ['hostname', 'registries', 'email', 'password'].filter(r => !config[r] || config[r].length === 0);
+      if (missingParams.length > 0) {
+          console.error(`Missing some required parameters: ${missingParams.join(', ')}`);
+          command.showHelp();
+          return;
       }
-    ]);
-  } catch (e) {
-    console.log("Aborting due to:", e);
-    return;
   }
 
   const options = {
-    baseURL: `https://${finalConfig.hostname}/artifactory/api/`,
+    baseURL: `https://${config.hostname}/artifactory/api/`,
     path: "/artifactory/api/npm/auth",
     auth: {
-      username: finalConfig.email,
-      password: finalConfig.password
+      username: config.email,
+      password: config.password
     }
   };
   const axiosInstance = axios.create(options);
 
-  if (finalConfig.useApiKey) {
+  console.log(`Fetching general registry information...`);
+
+  if (config.useApiKey) {
     try {
       // let's try to get an existing apikey
+      console.log(`Fetching api key...`);
       const requestResult = await axiosInstance.get("security/apiKey");
       options.auth.password = requestResult.data.apiKey;
     } catch (e) {
@@ -129,12 +196,13 @@ const main = async () => {
     finalResult = `${finalResult}\n${_authString}`;
   }
 
-  for (let i = 0; i < finalConfig.registries.length; i++) {
-    const [alias, repo] = finalConfig.registries[i]
+  for (let i = 0; i < config.registries.length; i++) {
+    const [alias, repo] = config.registries[i]
       .split("=>")
       .map(i => i.trim());
     const aliasWithoutAt = alias.slice(1);
 
+    console.log(`Fetching registry information for ${config.registries[i]}...`);
     const fssSpecificAuth = await axiosInstance.get(
       `npm/${repo}/auth/${aliasWithoutAt}`,
       {
@@ -145,7 +213,7 @@ const main = async () => {
     const registryRegExp = new RegExp(`^${alias}:registry\s*=.*\$`, "gm");
 
     const credsRegExp = new RegExp(
-      `^\/\/${finalConfig.hostname}:443/artifactory/api/npm/${repo}/:.*\$`,
+      `^\/\/${config.hostname}:443/artifactory/api/npm/${repo}/:.*\$`,
       "gm"
     );
     // Delete registry
@@ -159,7 +227,7 @@ const main = async () => {
   finalResult = finalResult.replace(/\n\s*\n/g, "\n");
   await writeFileP(outputFilePath, finalResult);
 
-  console.log("Success!");
+  console.log(`Successfully wrote to ${outputFilePath}.`);
   process.exit(0);
 };
 
